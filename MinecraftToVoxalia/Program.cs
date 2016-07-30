0be1376a3aaa401eca981764323b79960b6c1942
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2016-07-30 01:18:16 -0700

    Initial commit - empty console program

---
+++ MinecraftToVoxalia/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+
+namespace MinecraftToVoxalia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] files = Directory.GetFiles(Environment.CurrentDirectory, "*.mca", SearchOption.TopDirectoryOnly);
+            if (files.Length == 0)
+            {
+                Console.WriteLine("Please place the program directly amongst the region data (.mca files) you want to copy over.");
+            }
+        }
+    }
+}
